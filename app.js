8cd07ebce24a920a483c78855f9a9bc232df49e6
ianArlen <ian.street.fut@gmail.com>
2020-10-01 23:21:55 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+//Bring express
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//Bring in the routes
+
+
+//Setup Error Handlers
+const errorhandlers = require('./handlers/errorhandlers');
+app.use(errorhandlers.notFound);
+app.use(errorhandlers.mongoseErrors);
+app.use(errorhandlers.developmentErrors);
+
+module.exports = app;
